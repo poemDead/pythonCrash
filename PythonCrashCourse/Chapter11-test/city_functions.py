0519816faae2a1e848991dec780a0cc0c57bed46
poemDead <69440589+poemDead@users.noreply.github.com>
2020-10-18 18:19:42 +0900

    practice 11-2

--- PythonCrashCourse/Chapter11-test/city_functions.py
+++ PythonCrashCourse/Chapter11-test/city_functions.py
@@ -1,5 +1,8 @@
-def city_country(city,country):
-    full_city = f"{city}, {country}"
+def city_country(city,country,population=''):
+    if population:
+        full_city = f"{city}, {country} - population {population}"
+    else:
+        full_city = f"{city}, {country}"
     return full_city.title()
     
 
--- PythonCrashCourse/Chapter11-test/test_cities.py
+++ PythonCrashCourse/Chapter11-test/test_cities.py
@@ -7,6 +7,11 @@ class CitiesTestCase(unittest.TestCase):
         """能否正确输出如Tokyo, Japan这样的结果"""
         result_city = city_country('tokyo','japan')
         self.assertEqual(result_city,'Tokyo, Japan')
+    
+    def test_city_country_population(self):
+        """test when population entered"""
+        result_city = city_country('tokyo','japan','30000000')
+        self.assertEqual(result_city,'Tokyo, Japan - Population 30000000')
 
 if __name__ == "__main__":
     unittest.main()
